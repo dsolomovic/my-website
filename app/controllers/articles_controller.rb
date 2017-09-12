50798d69e327b0a4590d27cafd48364773def5ec
David Solomovic <davidsolomovic@unknown.local>
2017-09-12 21:39:57 +1000

    commit few changes in links until i get the blog up

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,26 @@
 class ArticlesController < ApplicationController
+
   def index
-  @articles = Article.find(params[:id])
-end
+   end
 
-def show
+   def create
+     @article = Article.new(article_params)
 
-end
+     @article.save
+     redirect_to @article
+   end
 
+
+   def new
+   end
+
+   def show
+     @article = Article.find(article_params)
+   end
+
+   private
+     def article_params
+       params.require(:article).permit(:title, :text)
+     end
 
 end
